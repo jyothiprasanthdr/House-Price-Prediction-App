50b7e57c22065cf952700941f778a58e87d07fe4
Jyothi Prasanth <jyothiprasanthdr@gmail.com>
2024-09-15 23:29:18 -0500

    Adding CORS to app

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 regmodel = pickle.load(open("model.pkl", "rb"))
